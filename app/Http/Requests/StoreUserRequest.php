d6b9af53e81fe4ecfb295d45e4e48a497dece3ed
Maksym Solomianyi <maxssolom@gmail.com>
2022-03-15 20:57:59 +0200

    Add form requests.

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email|unique:users,email',
+            'name' => 'required|min:3|max:25|unique:users,name',
+            'password' => [
+                'required',
+                Password::min(8)
+                    ->letters()
+                    ->mixedCase()
+                    ->numbers()
+                    ->symbols()
+            ],
+            'banned' => 'required|bool'
+        ];
+    }
+}
